54c228e6f0a98229ded8c7916b9675d04c3cc9e7
asinijayaweera <asinijayaweera03@gmail.com>
2026-06-27 21:18:38 +0530

    add layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,10 @@
 import "./globals.css";
+import { Rubik } from "next/font/google";
 
+const rubik = Rubik({ subsets: ["latin"] });
+
+//components
+import Navbar from "./components/Navbar";
 export const metadata = {
   title: "Dojo Helpdesk",
   description: "Helpdesk application",
@@ -8,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={rubik.className}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   );
 }
